9af4e4b1c14302e81a70636f1e8d3dc4d34cf07d
porygon <porygon@hermes.home>
2018-09-18 04:21:06 +0800

    Attempt to count number of deisred comment tags.

--- rpCommentScraper.py
+++ rpCommentScraper.py
@@ -1,9 +1,15 @@
 import requests
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup as BS
 url = 'https://us.battle.net/forums/en/wow/topic/20768997089'
 response = requests.get(url)
 html = response.content
 
-soup = BeautifulSoup(html)
+def num_appearances_of_tag(TopicPost-bodyContent, html):
+    soup = BS(html)
+    return len(soup.find_all(TopicPost-bodyContent)
+
+
+
+soup = BS(html)
 forum = soup.find('div', attrs={'class': 'TopicPost-bodyContent'})
 print forum.prettify()
