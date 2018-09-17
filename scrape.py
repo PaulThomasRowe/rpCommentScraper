146299cfc25b65423483f7438979723f212b253a
TheByrne <byrne.jaymes@gmail.com>
2018-09-17 11:55:25 -0700

    starting with basics

---
+++ scrape.py
@@ -0,0 +1,10 @@
+import requests
+from BeautifulSoup import BeautifulSoup
+url = 'https://us.battle.net/forums/en/wow/topic/20768997089'
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup(html)
+forum = soup.find('div', attrs={'class': 'Topic-content'})
+
+print forum.prettify()
