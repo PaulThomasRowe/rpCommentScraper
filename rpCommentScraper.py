43410975c678f895b74d7987af1ca031e5aa7350
TheByrne <byrne.jaymes@gmail.com>
2018-10-09 06:16:23 -0700

    Seperated all the needed text just need to re format it

--- rpCommentScraper.py
+++ rpCommentScraper.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as BS
+data=[]
+count=0
 url = 'https://us.battle.net/forums/en/wow/topic/20768997089'
 response = requests.get(url)
 html = response.content
@@ -7,5 +9,10 @@ html = response.content
 soup = BS(html , "lxml")
 
 
-for forum in soup.find_all('div', class_='TopicPost-bodyContent'):
-    print(forum.get_text())
+forum = soup.find_all('div', class_='TopicPost-bodyContent')
+
+for comment in forum:
+    comment = comment.text
+    print("")
+    print(comment)
+    print("")
