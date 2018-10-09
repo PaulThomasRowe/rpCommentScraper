164314cabf90008c788123270b3ce20e1fc09c80
TheByrne <byrne.jaymes@gmail.com>
2018-10-09 07:39:25 -0700

    It grabs a random post and a random comment just needs a little polish on the comment part

--- rpCommentScraper.py
+++ rpCommentScraper.py
@@ -1,18 +1,43 @@
 import requests
+import random
 from bs4 import BeautifulSoup as BS
 data=[]
+post=[]
 count=0
+max=0
+sectionUrl = 'https://us.battle.net/forums/en/wow/1011637/'
 url = 'https://us.battle.net/forums/en/wow/topic/20768997089'
-response = requests.get(url)
+response = requests.get(sectionUrl)
 html = response.content
 
 soup = BS(html , "lxml")
 
 
+section = soup.find_all('a', class_='ForumTopic')
+
+for links in section:
+    data.append(links.get('href'))
+
+max=len(data)
+
+chosen = random.randint(1,max+1)
+
+url = data[chosen]
+url = "https://us.battle.net"+url
+
+
+response = requests.get(url)
+html = response.content
+
+soup = BS(html , "lxml")
+
 forum = soup.find_all('div', class_='TopicPost-bodyContent')
 
 for comment in forum:
-    comment = comment.text
-    print("")
-    print(comment)
-    print("")
+    post.append(comment.text)
+
+max=len(post)
+
+chosen = random.randint(1,max+1)
+
+print(post[chosen])
